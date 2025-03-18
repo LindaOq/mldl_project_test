5015c24b651f72a8d21a7e28b75b3f823ddf2037
LindaOq <linoqv3@gmail.com>
2025-03-18 14:14:56 +0100

    Update datasets.py

--- data/datasets.py
+++ data/datasets.py
@@ -9,3 +9,7 @@ transform = T.Compose([
 
 tiny_imagenet_dataset_train = ImageFolder(root='tiny-imagenet/tiny-imagenet-200/train', transform=transform)
 tiny_imagenet_dataset_val = ImageFolder(root='tiny-imagenet/tiny-imagenet-200/val', transform=transform)
+
+import torch
+train_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_train, batch_size=32, shuffle=True)
+val_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_val, batch_size=32, shuffle=False)
